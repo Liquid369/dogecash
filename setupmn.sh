ea52ee5b3425d5c36dbaa5eb6d7b41406af42541
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-05-09 12:45:19 -0500

    Update setupmn.sh

--- setupmn.sh
+++ setupmn.sh
@@ -7,7 +7,7 @@ COIN_DAEMON='dogecashd'
 COIN_CLI='dogecash-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_REPO='https://github.com/Liquid369/dogecash'
-COIN_TGZ='https://transfer.sh/4F8FV/NixDG.7z'
+COIN_TGZ='https://transfer.sh/yLTNo/dogecash.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='DogeCash'
 COIN_PORT=16740 #Updated Port
@@ -117,9 +117,9 @@ function download_node() {
   compile_error
 #   tar xvzf $COIN_ZIP -C $COIN_PATH >/dev/null 2>&1
 # unzip dogecash.zip
-#tar xvzf dogecash-3.1.0-x86_64-linux-gnu.tar.gz
-#cd dogecash-3.1.0/bin
-7z e NixDG.7z
+tar -xzf dogecashd.tar.gz
+cd dogecashd/bin
+#7z e NixDG.7z
 chmod -R 775 *
 cp * $COIN_PATH
 cd ..
