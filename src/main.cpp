2f9c033183908c0d2504b7e45336a1e7d8596446
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-05-15 17:14:29 -0500

    Revert "Update main.cpp"
    This reverts commit dd571333d845232fcf40aeb1cb913be59d3608d3.

--- src/main.cpp
+++ src/main.cpp
@@ -3073,7 +3073,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     // two in the chain that violate it. This prevents exploiting the issue against nodes in their
     // initial block download.
     bool fEnforceBIP30 = (!pindex->phashBlock) || // Enforce on CreateNewBlock invocations which don't have a hash.
-                         !((pindex->nHeight == 0 && pindex->GetBlockHash() == uint256("00000c50b19d7f86f9052d2cfe92ab36a8b7f275496b07b0c5505db9e1784945")));
+                         !((pindex->nHeight == 0 && pindex->GetBlockHash() == uint256("0x01")));
 			   //||
                              //(pindex->nHeight == 91880 && pindex->GetBlockHash() == uint256("0x00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721")));
     if (fEnforceBIP30) {
